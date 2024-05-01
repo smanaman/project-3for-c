acf8f86afaca95cdfb3bd559582fb0ff5e1de7f9
smanaman <sanvatsman@gmail.com>
2024-04-30 18:02:44 -0700

    code

--- q3.c
+++ q3.c
@@ -14,13 +14,12 @@ int main() {
     }
 printf(" first_digit:%d\n",first_digit);
 
-    // // Extracting the last digit
     last_digit = number % 10;
 printf(" last_digit:%d\n",last_digit);
-    // // Calculating the sum
+  
     sum = first_digit + last_digit;
 
-    // // Output
+   
     printf("The sum of the first and the last digit: %d\n", sum);
 
     return 0;
